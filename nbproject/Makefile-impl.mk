96c7d91f967e06671301987dab0d624ae3212624
Nikita Romanovich <nikitarom@hotmail.com>
2023-01-02 14:21:36 +0300

    Update Makefile-impl.mk

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Вольт-амперметр
+PROJECTNAME=Clock with multiple alarms
 
 # Active Configuration
 DEFAULTCONF=default
