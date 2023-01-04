96f0a6ae7f258090dc518151071a05552d4c26ac
Nikita Romanovich <nikitarom@hotmail.com>
2023-01-04 22:01:10 +0300

    Create I2C.h

---
+++ I2C.h
@@ -0,0 +1,26 @@
+/* PCF8583
+ * File:   I2C.h
+ * Author: Никита
+ *
+ * Created on 4 января 2023 г., 20:03
+ */
+
+#ifndef I2C_H
+#define	I2C_H
+#define SCL TRISC1//
+#define SDA TRISC0 //TRISC4
+#define SCL_IN RC1 
+#define SDA_IN RC0
+#define _XTAL_FREQ 20000000
+#define dev_addrw 0b10100010 //запись
+#define dev_addrr 0b10100011 //чтение
+//----------------------------------------
+void i2c_start(void);
+void i2c_stop (void);
+void I2C_SendByte (unsigned char d);
+int I2C_ReadByte (void);
+int I2C_ReadByte_last (void);
+
+#endif	/* I2C_H */
+
+
