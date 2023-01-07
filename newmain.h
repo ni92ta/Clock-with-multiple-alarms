775cef1fb2a7c33459a6dae605504c8f3e47bf8d
Nikita Romanovich <nikitarom@hotmail.com>
2023-01-07 21:46:47 +0300

    Update newmain.h

--- newmain.h
+++ newmain.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 //--------------------------------------------------------------
-#define _XTAL_FREQ 20000000
+#define _XTAL_FREQ 3579000
 #include <xc.h>
 // CONFIG
-#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
 #pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
 #pragma config CP = OFF         // FLASH Program Memory Code Protection bit (Code protection off)
