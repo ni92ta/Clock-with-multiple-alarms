64dfef0309e08945972b8b01651cc3826c753498
Nikita Romanovich <nikitarom@hotmail.com>
2023-01-08 00:37:28 +0300

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -9,58 +9,106 @@ void LCD_delay() //
 int i;
 for(i=0;i<19;i++);
 }
+void sendbyteHalf(unsigned char c)
+{
+c <<= 4;    
+PORTB &= 0b00001111;
+PORTB |= c;
+e = 1;
+__delay_us(1);
+e = 0;
+__delay_us(50);
+}
+//--------------------------------------------------------------
 void sendbyte(unsigned char c, unsigned char mode) //
 {
-PORTB = c; //
+unsigned char hc = 0;  
+//PORTB = c; //
 if(mode == 0) rs = 0;
 else rs = 1;
-e = 0;
-LCD_delay();
-e = 1;
+hc = c >> 4;
+sendbyteHalf(hc);
+sendbyteHalf(c);
+//e = 1;
+//CD_delay();
+//e = 0;
 //c=0;
 }
 //--------------------------------------------------------------
+//--------------------------------------------------------------
+void LCD_Init()
+{
+  __delay_ms(50);
+  PORTA &= ~0x3;
+  PORTB &= ~0xF0;
+  __delay_ms(100);
+  sendbyteHalf(0x03);
+  __delay_us(4500);
+  sendbyteHalf(0x03);
+  __delay_us(4500);
+  sendbyteHalf(0x03);
+  __delay_us(200);
+  sendbyteHalf(0x02);
+  __delay_ms(1);
+  sendbyte(0x28, 0);//mode 4 bit, 2 lines (from our large display is 4 lines, font 5x8
+  //sendbyte(0x28, 0);//mode 4 bit, 2 lines (from our large display is 4 lines, font 5x8
+  sendbyte(0x0C,0);//Display ON, Cursor OFF, blink OFF
+  __delay_ms(1);
+  sendbyte(0x01,0);// Clear Display
+  __delay_ms(2);
+  sendbyte(0x06,0);// direction left to right
+  __delay_ms(1);
+  sendbyte(0x02,0);//Cursor reset
+  sendbyte(0X80,0);//SET POS LINE 0
+  __delay_ms(2);
+}
+//--------------------------------------------------------------
+/*
 void LCD_Init() //
 {
 __delay_ms(50); 
-sendbyte(0b00110000,0);//on
-__delay_ms(50); 
 
-sendbyte(0b00110000,0);//on
-__delay_us(40); 
 
 sendbyte(0b00110000,0);//on 
-__delay_us(40);
+__delay_us(37);
        
-sendbyte(0b00111100,0);//Function set: 8-bit bus mode,0b00111100,0
-//2-line display mode is set (2004 - 4-line), 5x7 dots format
-__delay_us(40);
+sendbyte(0b00101000,0);
+//            ||+-----1:Шрифт 5*10 точек, 0: 5*8 точек
+//            |+------1:Две строки, 0:Одна строка
+//            +-------1:Интерфейс 8 бит, 0:4 бита
+__delay_us(37);
   
-//sendbyte(0b00000000,0);//
-sendbyte(0b00001100,0);//display on, cursor on, blink on
-__delay_us(40);
+sendbyte(0b00001100,0);
+//              ||+---1:Включение, 0:Отключение мигания позиции курсора
+//              |+----1:Включение, 0:Отключение курсора
+//              +-----1:Включение, 0:Отключение дисплея
+__delay_us(37);
 
-//sendbyte(0b00000000,0);//
-sendbyte(0b00000001,0);//
-__delay_ms(2);
+sendbyte(0b00000001,0);//очистка дисплея
+//__delay_ms(2);
    
-//sendbyte(0b00000000,0);//
 sendbyte(0b00000111,0);// increment mode, entire shift on
-__delay_us(40);
+__delay_us(37);
   
-sendbyte(0b00010100,0);// display shift (cursor shift), right shift
-__delay_us(40);
+sendbyte(0b00010100,0);
+//             |+-----1:Сдвиг вправо, 0:Сдвиг влево
+//             +------1:Сдвиг дисплея, 0:Сдвиг курсора
+__delay_us(37);
   
-sendbyte(0b00000010,0);//
-__delay_us(40);
+sendbyte(0b00000110,0);//direction left to right
+//               |+---1:Разрешить, 0:Запретить сдвиг дисплея, двигать курсор
+//               +----1:Приращение, 0:Уменьшение курсора
+__delay_us(37);
+
+sendbyte(0b00000010,0);//Счётчик DDRAM в 0
+__delay_ms(2);
   
-sendbyte(0b01000000,0);//sets CGRAM address
-__delay_us(40);
-   
-sendbyte(0X06,0);//direction left to right
+sendbyte(0b01000000,0);//Установить адрес CGRAM в 0
+__delay_us(37);
   
-sendbyte(0b10000000,0);//SET POS LINE 0
-}
+sendbyte(0b10000000,0);//Установить адрес DDRAM в 0
+__delay_us(37);
+}*/
 /*//--------------------------------------------------------------
 void LCD_Clear(void)
 {
@@ -91,3 +139,16 @@ i++;
 }
 }
 //--------------------------------------------------------------
+/*
+ void sendbyte(unsigned char c, unsigned char mode) //
+{
+PORTB = c; //
+if(mode == 0) rs = 0;
+else rs = 1;
+e = 0;
+LCD_delay();
+e = 1;
+//c=0;
+}
+//--------------------------------------------------------------
+ */
